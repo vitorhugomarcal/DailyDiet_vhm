5a1d32019c241c7202efec843bb309fa65566de0
vitorhugomarcal <vitorhugomarcal@gmail.com>
2023-02-24 20:53:43 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components'
+
+import { Nunito_400Regular, Nunito_500Medium, Nunito_700Bold, useFonts } from '@expo-google-fonts/nunito'
+
+import theme from '@theme/index';
+import { Loading } from '@components/Loading';
+import { Routes } from '@routes/index';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_700Bold
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+      { fontsLoaded ? <Routes /> : <Loading />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GREEN_700: '#639339',
+    GREEN_500: '#CBE4B4',
+    GREEN_300: '#E5F0DB',
+
+    RED_LIGHT: '#F4E6E7',
+    RED_MID: '#F3BABD',
+    RED_DARK: '#BF3B44',
+
+    GRAY_700: '#1B1D1E',
+    GRAY_600: '#333638',
+    GRAY_500: '#5C6265',
+    GRAY_400: '#B9BBBC',
+    GRAY_300: '#DDDEDF',
+    GRAY_200: '#EFF0F0',
+    GRAY_100: '#FAFAFA'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Roboto_400Regular',
+    BOLD: 'Roboto_700Bold'
+  },
+  FONT_SIZE: {
+    XM: '12px',
+    SM: '14px',
+    MD: '16px',
+    LG: '18px',
+    XL: '24px',
+    XXL: '32px'
+  }
+};
